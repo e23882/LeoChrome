76438139ebdd6daf8813f249a83d938de350263a
leo <e23882@gmail.com>
2025-12-25 17:34:08 +0800

    chroe: remove unused code

--- MahAppBase/obj/x86/Debug/net8.0-windows/MahAppBase.AssemblyInfo.cs
+++ MahAppBase/obj/x86/Debug/net8.0-windows/MahAppBase.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MahAppBase")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3126d6107ec083d9d7445f449120b57565a2595a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+969f4f7c03bf809c141e792d157a1d7099a1cae8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MahAppBase")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MahAppBase")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
